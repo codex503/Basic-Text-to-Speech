b45575bbc11461911bb16a34bc1df12016b7b6f5
Derek Lee <dleedev7@gmail.com>
2017-09-25 23:22:19 -0400

    removal of unnecessary comments

--- voice.py
+++ voice.py
@@ -1,15 +1,14 @@
 
 
-# # prompt user for instructions
-# print "What would you like me to say?"
-# # create a variable to store input to string
-# speech = raw_input()
-# # 
-# print speech
-
+# import cross-platform module
 import pyttsx
+
+# set initialization to variable
 engine = pyttsx.init()
+
+# prompt for user input
 print "What would you like me to say?"
+
 
 engine.say(raw_input())
 engine.runAndWait()
